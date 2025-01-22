06ec822a554801d14e350e624f7f23a512ae19e7
jeloFojas <jelo.fojas@deped.gov.ph>
2025-01-22 08:01:13 +0000

    commit

--- main.cs
+++ main.cs
@@ -1,7 +1,18 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] names = {"Lanoy",
+                        "Castro",
+                        "Casas",
+                        "Catapia",
+                        "Ramos"};
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine(names[i]);
+        }
+    }
 }
